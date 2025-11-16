184910d58ec6cf006a07c23827d0ab27fe4be532
Yusuf Harby <youssef.srour789@gmail.com>
2025-11-16 08:02:42 +0200

    feat: Add initial version of home screen

---
+++ lib/app_colors.dart
@@ -0,0 +1,8 @@
+import 'dart:ui';
+abstract class AppColors {
+  static const Color primary = Color(0xFF29384D);
+  static const Color secondary = Color(0xFFFFF1D4);
+  static const Color textfield = Color(0xFFE2F4F6);
+  static const Color white = Color(0xFFFFFFFF);
+  static const Color red = Color(0xFFF93E3E);
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,6 @@
+import 'package:contact/app_colors.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 
 void main() {
   runApp(ContactApp());
@@ -11,9 +13,33 @@ class ContactApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: Scaffold(
-        appBar: AppBar(title: Text('Contact List')),
-        body: Center(child: Text('Welcome to the Contact List App!')),
+      home: ScreenUtilInit(
+        designSize: const Size(402, 874),
+        minTextAdapt: true,
+        splitScreenMode: true,
+        child: Scaffold(
+          backgroundColor: AppColors.primary,
+          appBar: AppBar(
+            backgroundColor: AppColors.primary,
+            title: Image.asset(
+              'assets/icons/logo.png',
+              width: 117.w,
+              height: 39.h,
+            ),
+          ),
+          body: Center(child: Text('Welcome to the Contact List App!')),
+          floatingActionButton: SizedBox(
+            width: 56.w,
+            height: 56.w,
+            child: FloatingActionButton(
+              onPressed: () {
+                // Action to add a new contact
+              },
+              backgroundColor: AppColors.secondary,
+              child: Icon(Icons.add, size: 29.w, color: AppColors.primary),
+            ),
+          ),
+        ),
       ),
     );
   }
